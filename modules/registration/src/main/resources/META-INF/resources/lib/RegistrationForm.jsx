ed7e31b83b2ce23ec520b2f4e822b2d7885b5a38
Brian Heise <brian.heise@liferay.com>
2021-10-26 18:27:56 +0900

    deleted unusued components

--- modules/registration/src/main/resources/META-INF/resources/lib/RegistrationForm.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/RegistrationForm.jsx
@@ -1,39 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-import { ClayInput } from '@clayui/form';
 import FormSection from './components/FormSection';
 import TwoColGroup from './components/TwoColGroup';
-import GenderSelect from './components/GenderSelect';
-import BirthdaySelector from './components/BirthdaySelector';
+import Select from './components/Select';
+import Datepicker from './components/Datepicker';
+import Input from './components/Input';
 
 export default function RegistrationForm() {
+    const genderOptions = [
+        { label: "Male", value: "male" },
+        { label: "Female", value: "female" }
+    ];
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+    const emailRef = useRef(null);
+    const usernameRef = useRef(null);
+    const birthdayRef = useRef(null);
+    const genderRef = useRef(null);
+    const passwordRef = useRef(null);
+    const confrimPasswordRef = useRef(null);
+
     return (
         <div>
             <h2>User Registration</h2>
             <FormSection heading="Basic Info">
                 <TwoColGroup>
-                    <label htmlFor="firstName">First Name</label>
-                    <ClayInput id="firstName" placeholder="first name" type="text"></ClayInput>
-                    <label htmlFor="lastName">Last Name</label>
-                    <ClayInput id="lastName" placeholder="last name" type="text"></ClayInput>
+                    <Input label="First Name" reference={firstNameRef} />
+                    <Input label="Last Name" reference={lastNameRef} />
                 </TwoColGroup>
                 <TwoColGroup>
-                    <label htmlFor="email">Email</label>
-                    <ClayInput id="email" placeholder="email" type="email"></ClayInput>
-                    <label htmlFor="username">Username</label>
-                    <ClayInput id="username" placeholder="username" type="text"></ClayInput>
+                    <Input label="Email" reference={emailRef} type="email" />
+                    <Input label="Username" reference={usernameRef} />
                 </TwoColGroup>
                 <TwoColGroup>
-                    <label htmlFor="genderSelect">Gender</label>
-                    <GenderSelect id="genderSelect" />
-                    <label htmlFor="birthday">Birthday</label>
-                    <BirthdaySelector />
+                    <Select label="Gender" options={genderOptions} reference={genderRef} />
+                    <Datepicker label="Birthday" reference={birthdayRef} />
                 </TwoColGroup>
                 <TwoColGroup>
-                    <label htmlFor="password">Password</label>
-                    <ClayInput id="password" placeholder="password" type="password"></ClayInput>
-                    <label htmlFor="confirmPassword">Confirm password</label>
-                    <ClayInput id="confirmPassword" placeholder="confirm your password" type="password"></ClayInput>
+                    <Input label="Password" reference={passwordRef} type="password" />
+                    <Input label="Confirm Password" reference={confrimPasswordRef} type="password" />
                 </TwoColGroup>
             </FormSection>
         </div>
