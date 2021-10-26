9d03f558567745c83166cc01b9c9a48479617b41
Brian Heise <brian.heise@liferay.com>
2021-10-26 18:28:55 +0900

    changed to assume the group includes a label

--- modules/registration/src/main/resources/META-INF/resources/lib/components/TwoColGroup.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/TwoColGroup.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import ClayForm, { ClayInput } from '@clayui/form';
+import ClayForm from '@clayui/form';
 import ClayLayout from '@clayui/layout';
 
 export default function TwoColGroup({ children }) {
     if (children.length > 4) throw new Error("TwoColGroup only accepts two child components");
 
-    const [label1, input1, label2, input2] = children;
+    const [input1, input2] = children;
     return (
         <ClayForm.Group className="form-group-sm">
             <ClayLayout.Row justify="start">
                 <ClayLayout.Col md={4} lg={4} sm={8} xs={12} >
-                    {label1}
                     {input1}
                 </ClayLayout.Col>
                 <ClayLayout.Col md={4} lg={4} sm={8} xs={12} >
-                    {label2}
                     {input2}
                 </ClayLayout.Col>
             </ClayLayout.Row>
