b2a3f7e276db3c631ffd901709cd7fed7a2578cc
Brian Heise <brian.heise@liferay.com>
2021-10-26 18:28:37 +0900

    fixed a bug in toCamel

--- modules/registration/src/main/resources/META-INF/resources/lib/utils/utils.js
+++ modules/registration/src/main/resources/META-INF/resources/lib/utils/utils.js
@@ -1,7 +1,7 @@
-export const toCamel = (string) => string.split(' ').map(camelMap(word, index)).join('');
+export const toCamel = (string) => string.split(' ').map((word, index) => camelMap(word, index)).join('');
 export const isValidDate = (date) => date instanceof Date && !isNaN(date);
 
-const capitalize = (word) => [...word[0].toUpperCase(), ...word.slice(1)].join();
+const capitalize = word => [...word[0].toUpperCase(), ...word.slice(1)].join();
 const toLowerCase = word => word.toLowerCase();
 const firstOrElse = (firstCallback, secondCallback) => (item, index) => index === 0 ? firstCallback(item) : secondCallback(item);
 const camelMap = firstOrElse(toLowerCase, capitalize);
