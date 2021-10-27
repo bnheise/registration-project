bd7167880da771fe503b89154f7f9826f3864379
Brian Heise <brian.heise@liferay.com>
2021-10-27 10:48:17 +0900

    formatted and added proptypes

--- modules/registration/src/main/resources/META-INF/resources/lib/components/Input.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/Input.jsx
@@ -1,19 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { ClayInput } from "@clayui/form";
 import { toCamel } from "../utils/utils";
 
 const Input = ({ label, reference, type = "text" }) => {
-    const camelLabel = toCamel(label);
-    return (
-        <div>
-            <label htmlFor={camelLabel}>{label}</label>
-            <ClayInput
-                id={camelLabel}
-                ref={reference}
-                placeholder={label.toLowerCase()}
-                type={type} />
-        </div>
-    )
-}
+  const camelLabel = toCamel(label);
+  return (
+    <div>
+      <label htmlFor={camelLabel}>{label}</label>
+      <ClayInput
+        id={camelLabel}
+        ref={reference}
+        placeholder={label.toLowerCase()}
+        type={type}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  reference: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]).isRequired,
+  type: PropTypes.oneOf(["text", "password", "email"]),
+};
+
+Input.defaultProps = {
+  type: "text",
+};
 
 export default Input;
