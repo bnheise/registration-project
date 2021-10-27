a3b30df23d670b3fd9ef43e2892bd80b2578f470
Brian Heise <brian.heise@liferay.com>
2021-10-27 10:48:39 +0900

    formatted and added proptypes

--- modules/registration/src/main/resources/META-INF/resources/lib/components/TwoColGroup.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/TwoColGroup.jsx
@@ -1,21 +1,36 @@
 import React from "react";
-import ClayForm from '@clayui/form';
-import ClayLayout from '@clayui/layout';
+import PropTypes from "prop-types";
+import ClayForm, { ClayInput } from "@clayui/form";
+import ClayLayout from "@clayui/layout";
 
-export default function TwoColGroup({ children }) {
-    if (children.length > 4) throw new Error("TwoColGroup only accepts two child components");
+const TwoColGroup = ({ children }) => {
+  if (children.length > 2)
+    throw new Error("TwoColGroup only accepts two child components");
 
-    const [input1, input2] = children;
-    return (
-        <ClayForm.Group className="form-group-sm">
-            <ClayLayout.Row justify="start">
-                <ClayLayout.Col md={4} lg={4} sm={8} xs={12} >
-                    {input1}
-                </ClayLayout.Col>
-                <ClayLayout.Col md={4} lg={4} sm={8} xs={12} >
-                    {input2}
-                </ClayLayout.Col>
-            </ClayLayout.Row>
-        </ClayForm.Group>
-    )
-}
+  const [input1, input2] = children;
+  return (
+    <ClayForm.Group className="form-group-sm">
+      <ClayLayout.Row justify="start">
+        <ClayLayout.Col md={4} lg={4} sm={8} xs={12}>
+          {input1}
+        </ClayLayout.Col>
+        <ClayLayout.Col md={4} lg={4} sm={8} xs={12}>
+          {input2}
+        </ClayLayout.Col>
+      </ClayLayout.Row>
+    </ClayForm.Group>
+  );
+};
+
+TwoColGroup.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+TwoColGroup.defaultProps = {
+  children: [ClayInput, ClayInput],
+};
+
+export default TwoColGroup;
