b844521fdd35250bdb9684776da8fc0bff2b24b8
Brian Heise <brian.heise@liferay.com>
2021-10-27 10:48:06 +0900

    formatted and added proptypes

--- modules/registration/src/main/resources/META-INF/resources/lib/components/FormSection.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/FormSection.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+import ClayForm from "@clayui/form";
 
-import ClayForm from '@clayui/form';
+const FormSection = ({ heading, children }) => {
+  return (
+    <div>
+      <h3>{heading}</h3>
+      <ClayForm>{children}</ClayForm>
+    </div>
+  );
+};
 
-export default function FormSection({ heading, children }) {
-    return (
-        <div>
-            <h3>{heading}</h3>
-            <ClayForm>
-                {children}
-            </ClayForm>
-        </div>
-    )
-}
+FormSection.propTypes = {
+  heading: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+FormSection.defaultProps = {
+  children: ClayForm,
+};
+
+export default FormSection;
