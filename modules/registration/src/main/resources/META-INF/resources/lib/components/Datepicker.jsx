57ed77a23e6088fcd5cd9a7531766fb1d25b67d8
Brian Heise <brian.heise@liferay.com>
2021-10-27 10:47:50 +0900

    formatted

--- modules/registration/src/main/resources/META-INF/resources/lib/components/Datepicker.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/Datepicker.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
-
-import ClayDatePicker from '@clayui/date-picker';
+import PropTypes from "prop-types";
+import ClayDatePicker from "@clayui/date-picker";
 import { toCamel } from "../utils/utils";
 
 const DatePicker = ({ label, reference }) => {
-    const [value, setValue] = useState("");
-    const handleChange = ({ target: { value: inputValue } }) => {
-        setValue(inputValue)
-    }
-    const camelLabel = toCamel(label);
+  const [value, setValue] = useState("");
+  const handleChange = ({ target: { value: inputValue } }) => {
+    setValue(inputValue);
+  };
+  const camelLabel = toCamel(label);
 
-    return (
-        <div>
-            <label htmlFor={camelLabel}>{label}</label>
-            <ClayDatePicker
-                id={camelLabel}
-                onValueChange={setValue}
-                placeholder="YYYY-MM-DD"
-                value={value}
-                onChange={handleChange}
-                ref={reference}
-                spritemap={`${Liferay.ThemeDisplay.getCDNBaseURL()}/documents/37341/0/icons.svg`}
-                years={{
-                    end: 2024,
-                    start: 1997
-                }}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <label htmlFor={camelLabel}>{label}</label>
+      <ClayDatePicker
+        id={camelLabel}
+        onValueChange={setValue}
+        placeholder="YYYY-MM-DD"
+        value={value}
+        onChange={handleChange}
+        ref={reference}
+        spritemap={`${Liferay.ThemeDisplay.getCDNBaseURL()}/documents/37341/0/icons.svg`}
+        years={{
+          end: 2024,
+          start: 1997,
+        }}
+      />
+    </div>
+  );
+};
+
+DatePicker.propTypes = {
+  label: PropTypes.string.isRequired,
+  reference: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]).isRequired,
+};
 
 export default DatePicker;
