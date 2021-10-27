ebab85178c9936f19441e2b860f1dbfa87aa5b82
Brian Heise <brian.heise@liferay.com>
2021-10-27 10:48:29 +0900

    formatted and added proptypes

--- modules/registration/src/main/resources/META-INF/resources/lib/components/Select.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/Select.jsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { ClaySelect } from '@clayui/form';
+import PropTypes from "prop-types";
+import { ClaySelect } from "@clayui/form";
 import { toCamel } from "../utils/utils";
 
 const Select = ({ label, options, reference }) => {
-    const camelLabel = toCamel(label);
-    return (
-        <div>
-            <label htmlFor={camelLabel}>{label}</label>
-            <ClaySelect ref={reference} id={camelLabel} aria-label="Select Label" >
-                {options.map(item => (
-                    <ClaySelect.Option
-                        key={item.value}
-                        label={item.label}
-                        value={item.value}
-                    />
-                ))}
-            </ClaySelect>
-        </div>
-    )
-}
+  const camelLabel = toCamel(label);
+  return (
+    <div>
+      <label htmlFor={camelLabel}>{label}</label>
+      <ClaySelect ref={reference} id={camelLabel} aria-label="Select Label">
+        {options.map((item) => (
+          <ClaySelect.Option
+            key={item.value}
+            label={item.label}
+            value={item.value}
+          />
+        ))}
+      </ClaySelect>
+    </div>
+  );
+};
+
+Select.propTypes = {
+  label: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string,
+    })
+  ).isRequired,
+  reference: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]).isRequired,
+};
 
 export default Select;
