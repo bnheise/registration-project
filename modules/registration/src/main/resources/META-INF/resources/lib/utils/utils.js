e537e8d15c87f3f3dc3bdb4df92ca6a5d61a59de
Brian Heise <brian.heise@liferay.com>
2021-10-27 14:03:11 +0900

    added utility function to generate change handlers for the form

--- modules/registration/src/main/resources/META-INF/resources/lib/utils/utils.js
+++ modules/registration/src/main/resources/META-INF/resources/lib/utils/utils.js
@@ -1,9 +1,27 @@
 const capitalize = (word) =>
-  [...word[0].toUpperCase(), ...word.slice(1)].join();
+  [...word[0].toUpperCase(), ...word.slice(1)].join("");
+
 const toLowerCase = (word) => word.toLowerCase();
+
 const firstOrElse = (firstCallback, secondCallback) => (item, index) =>
   index === 0 ? firstCallback(item) : secondCallback(item);
+
 const camelMap = firstOrElse(toLowerCase, capitalize);
+
+const genChangeHandler =
+  (stateSetter, stateObject, currentKey) =>
+  ({ target: value }) =>
+    stateSetter({ ...stateObject, [currentKey]: value });
+
+export const genChangeHandlers = (stateObject, stateSetter) =>
+  Object.keys(stateObject).reduce((allHandlers, currentKey) => {
+    allHandlers[`handle${capitalize(currentKey)}Change`] = genChangeHandler(
+      stateSetter,
+      stateObject,
+      currentKey
+    );
+    return allHandlers;
+  }, {});
 
 export const toCamel = (string) =>
   string
