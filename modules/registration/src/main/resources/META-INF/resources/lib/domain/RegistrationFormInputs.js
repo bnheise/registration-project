7def01aa35c0979ccc08dd6f75ecce47ac6b5537
Brian Heise <brian.heise@liferay.com>
2021-10-27 14:02:01 +0900

    created a class to manage the registration form inputs

---
+++ modules/registration/src/main/resources/META-INF/resources/lib/domain/RegistrationFormInputs.js
@@ -0,0 +1,21 @@
+export default class RegistrationFormInputs {
+  constructor({
+    screenName,
+    emailAddress,
+    firstName,
+    lastName,
+    gender,
+    birthday,
+    password,
+    passwordConfirmation,
+  } = {}) {
+    this.screenName = screenName || "";
+    this.emailAddress = emailAddress || "";
+    this.firstName = firstName || "";
+    this.lastName = lastName || "";
+    this.gender = gender || "male";
+    this.birthday = birthday;
+    this.password1 = password || "";
+    this.password2 = passwordConfirmation || "";
+  }
+}
