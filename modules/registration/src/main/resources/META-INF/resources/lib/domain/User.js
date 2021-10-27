b8523b79ba048c9633ae11a180d5af9a91d6c085
Brian Heise <brian.heise@liferay.com>
2021-10-27 14:02:46 +0900

    added utility class to convert form inputs to an API friendly form

--- modules/registration/src/main/resources/META-INF/resources/lib/domain/User.js
+++ modules/registration/src/main/resources/META-INF/resources/lib/domain/User.js
@@ -1,22 +1,35 @@
 import { isValidDate } from "../utils/utils";
 
 export default class User {
-    constructor({ screenName, emailAddress, firstName, lastName, gender, birthday, password, passwordConfirmation }) {
-        if (gender !== 'male' || gender !== 'female') throw new Error(`Expected 'gender' property of User constructor object to be 'male' or 'female' but insead got ${gender}`)
-        const male = gender.toLowerCase() === 'male';
+  constructor({
+    screenName,
+    emailAddress,
+    firstName,
+    lastName,
+    gender,
+    birthday,
+    password,
+    passwordConfirmation,
+  }) {
+    if (gender !== "male" || gender !== "female")
+      throw new Error(
+        `Expected 'gender' property of User constructor object to be 'male' or 'female' but insead got ${gender}`
+      );
+    const male = gender.toLowerCase() === "male";
 
-        const parsedBirthday = new Date(birthday);
-        if (!isValidDate(parsedBirthday)) throw new Error('Invalid date passed to User as birthday');
+    const parsedBirthday = new Date(birthday);
+    if (!isValidDate(parsedBirthday))
+      throw new Error("Invalid date passed to User as birthday");
 
-        this.screenName = screenName;
-        this.emailAddress = emailAddress;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.male = male
-        this.birthdayMonth = parsedBirthday.getMonth();
-        this.birthdayDay = parsedBirthday.getDate();
-        this.birthdayYear = parsedBirthday.getFullYear();
-        this.password1 = password;
-        this.password2 = passwordConfirmation;
-    }
+    this.screenName = screenName;
+    this.emailAddress = emailAddress;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.male = male;
+    this.birthdayMonth = parsedBirthday.getMonth();
+    this.birthdayDay = parsedBirthday.getDate();
+    this.birthdayYear = parsedBirthday.getFullYear();
+    this.password1 = password;
+    this.password2 = passwordConfirmation;
+  }
 }
