86a766fba27d0f6b7a2084b9a18dd55828478d1a
Brian Heise <brian.heise@liferay.com>
2021-10-29 13:48:12 +0900

    implemented error message handling for input

--- modules/registration/src/main/resources/META-INF/resources/lib/components/Input.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ClayInput } from "@clayui/form";
+import ClayForm, { ClayInput } from "@clayui/form";
 import { toCamel } from "../utils/utils";
 import Label from "./Label";
 
@@ -10,11 +10,14 @@ const Input = ({
   handleChange,
   type = "text",
   isRequired = false,
+  errors = "",
 }) => {
+  console.log(errors);
   const camelLabel = toCamel(label);
-
+  errors = typeof errors === "string" && errors.length > 0 ? [errors] : errors;
+  console.log(errors.length);
   return (
-    <div>
+    <ClayForm.Group className={`${errors.length > 0 ? "has-error" : null}`}>
       <Label text={label} isRequired={isRequired} />
       <ClayInput
         id={camelLabel}
@@ -23,7 +26,14 @@ const Input = ({
         placeholder={label.toLowerCase()}
         type={type}
       />
-    </div>
+      {errors.length > 0 ? (
+        <ClayForm.FeedbackGroup>
+          {errors.map((error, index) => (
+            <ClayForm.FeedbackItem key={index}>{error}</ClayForm.FeedbackItem>
+          ))}
+        </ClayForm.FeedbackGroup>
+      ) : null}
+    </ClayForm.Group>
   );
 };
 
@@ -33,11 +43,16 @@ Input.propTypes = {
   handleChange: PropTypes.func.isRequired,
   type: PropTypes.oneOf(["text", "password", "email"]),
   isRequired: PropTypes.bool,
+  errors: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
 };
 
 Input.defaultProps = {
   type: "text",
   isRequired: false,
+  errors: "",
 };
 
 export default Input;
