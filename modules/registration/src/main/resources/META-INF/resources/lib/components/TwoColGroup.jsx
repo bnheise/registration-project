a83837deece056f359701be0d5283aaa40f03ba3
Brian Heise <brian.heise@liferay.com>
2021-10-29 13:48:38 +0900

    removed form group from two col group

--- modules/registration/src/main/resources/META-INF/resources/lib/components/TwoColGroup.jsx
+++ modules/registration/src/main/resources/META-INF/resources/lib/components/TwoColGroup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import ClayForm, { ClayInput } from "@clayui/form";
+import { ClayInput } from "@clayui/form";
 import ClayLayout from "@clayui/layout";
 
 const TwoColGroup = ({ children }) => {
@@ -9,16 +9,14 @@ const TwoColGroup = ({ children }) => {
 
   const [input1, input2] = children;
   return (
-    <ClayForm.Group className="form-group-sm">
-      <ClayLayout.Row justify="start">
-        <ClayLayout.Col md={4} lg={4} sm={8} xs={12}>
-          {input1}
-        </ClayLayout.Col>
-        <ClayLayout.Col md={4} lg={4} sm={8} xs={12}>
-          {input2}
-        </ClayLayout.Col>
-      </ClayLayout.Row>
-    </ClayForm.Group>
+    <ClayLayout.Row justify="start">
+      <ClayLayout.Col md={4} lg={4} sm={8} xs={12}>
+        {input1}
+      </ClayLayout.Col>
+      <ClayLayout.Col md={4} lg={4} sm={8} xs={12}>
+        {input2}
+      </ClayLayout.Col>
+    </ClayLayout.Row>
   );
 };
 
