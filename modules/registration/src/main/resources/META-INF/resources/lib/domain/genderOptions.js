ea1c81c74e7e01eac9555bb821cc8f4a5b828d4a
Brian Heise <brian.heise@liferay.com>
2021-11-02 17:40:04 +0900

    deleted old versions of the modules

--- modules/registration/src/main/resources/META-INF/resources/lib/domain/genderOptions.js
+++
@@ -1,4 +0,0 @@
-export const genderOptions = [
-  { label: "Male", value: "male" },
-  { label: "Female", value: "female" },
-];
--- modules/registration/src/main/resources/META-INF/resources/lib/index.es.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import RegistrationPortlet from "./RegistrationPortlet";
-
-export default function (elementId) {
-  ReactDOM.render(<RegistrationPortlet />, document.getElementById(elementId));
-}
