7877294685ad727cb4efeb233ae29bb0c16a1151
Brian Heise <brian.heise@liferay.com>
2021-11-04 19:35:24 +0900

    fixed checkbox bug so it updates state properly

--- modules/signup/signup-web/src/components/Checkbox.tsx
+++ modules/signup/signup-web/src/components/Checkbox.tsx
@@ -1,20 +1,28 @@
-import React, { FC } from "react";
-import { ClayCheckbox } from "@clayui/form";
+import React, { ChangeEventHandler, FC } from "react";
+import ClayForm, { ClayCheckbox } from "@clayui/form";
 import { toCamel } from "../utils/utils";
+import useErrors from "../hooks/useError";
 
 interface ICheckboxProps {
   label: string,
-  handler: React.Dispatch<React.SetStateAction<boolean>>
-  value: boolean
+  handleChange: ChangeEventHandler,
+  value?: boolean,
+  errors?: string | string[]
 }
 
-const Checkbox: FC<ICheckboxProps> = ({ label, handler, value = false }) => {
+const Checkbox: FC<ICheckboxProps> = ({ label, handleChange, value = false, errors }) => {
   const camelLabel = toCamel(label);
+  if (typeof value === "string") value = value === "true";
+  const [className, errorUI] = useErrors(errors);
   return (
-    <div>
+    <ClayForm.Group className={className}>
       <label htmlFor={camelLabel}>{label}</label>
-      <ClayCheckbox checked={value} onChange={() => handler((val) => !val)} />
-    </div>
+      <ClayCheckbox checked={value} onChange={(event) => {
+          event.target.value = event.target.value === "true" ? "false" : "true";
+          handleChange(event);
+        }} />
+      {errorUI}
+    </ClayForm.Group>
   );
 };
 
