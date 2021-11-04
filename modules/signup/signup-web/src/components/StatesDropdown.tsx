2e44ce7868a66d2be9d618685ff075f2e3e9d24c
Brian Heise <brian.heise@liferay.com>
2021-11-04 19:35:50 +0900

    added state errors

--- modules/signup/signup-web/src/components/StatesDropdown.tsx
+++ modules/signup/signup-web/src/components/StatesDropdown.tsx
@@ -1,41 +1,58 @@
-import React, { ChangeEventHandler, FC, ReactElement, useEffect, useState } from "react";
-import { ClaySelect } from "@clayui/form";
+import React, {
+  ChangeEventHandler,
+  FC,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
+import ClayForm, { ClaySelect } from "@clayui/form";
 import { RegionDTO } from "../domain/RegionDTO";
 import Label from "./Label";
+import useErrors from "../hooks/useError";
 
 interface IStatesDropdownProps {
-    value: number | undefined,
-    handleChange?: ChangeEventHandler,
+  value: number | undefined;
+  handleChange?: ChangeEventHandler;
+  errors?: string | string[];
 }
 
-const StatesDropdown: FC<IStatesDropdownProps> = ({value, handleChange}): ReactElement => {
-  const [states, setStates] = useState<RegionDTO[]>([]);
+const StatesDropdown: FC<IStatesDropdownProps> = ({
+  value,
+  handleChange,
+  errors
+}): ReactElement => {
+  const [states, setStates] = useState<RegionDTO[]>([new RegionDTO({regionId: -1, regionCode: "", countryId: -1, name: ""})]);
+  const [className, errorUI] = useErrors(errors);
   useEffect(() => {
-      let mounted = true;
-        Liferay.Service(
-            "/signup.signup/get-states",
-            (reponse: RegionDTO[]) => {
-              console.log(reponse);
-              if (mounted) {
-                setStates(reponse);
-              }
-            },
-            (error) => console.log(error)
-          );
-      return () => {mounted = false};
+    let mounted = true;
+    Liferay.Service(
+      "/signup.signup/get-states",
+      (reponse: RegionDTO[]) => {
+        if (mounted) {
+          setStates([...states, ...reponse]);
+        }
+      },
+      (error) => console.error(error)
+    );
+    return () => {
+      mounted = false;
+    };
   }, []);
-  
+
   return (
-    <div>
+    <ClayForm.Group className={className}>
       <Label text="State" isRequired={true} />
-      <ClaySelect onChange={handleChange} value={value}>{states.map((state) => (
-        <ClaySelect.Option
-          key={state.regionId}
-          label={state.name}
-          value={state.regionId}
-        />
-      ))}</ClaySelect>
-    </div>
+      <ClaySelect onChange={handleChange} value={value}>
+        {states.map((state) => (
+          <ClaySelect.Option
+            key={state.regionId}
+            label={state.name}
+            value={state.regionId}
+          />
+        ))}
+      </ClaySelect>
+      {errorUI}
+    </ClayForm.Group>
   );
 };
 
