331012b3e7d908549aa482f2f007527ac9f9532a
Brian Heise <brian.heise@liferay.com>
2021-11-22 16:41:27 +0900

    implemented error display

--- modules/search/search-web-input/src/components/SearchForm/SearchFormDisplay.tsx
+++ modules/search/search-web-input/src/components/SearchForm/SearchFormDisplay.tsx
@@ -3,15 +3,17 @@ import Button from '@clayui/button'
 import ClayForm, { ClayInput } from '@clayui/form'
 
 interface Props {
-    handleChange: ChangeEventHandler<HTMLInputElement>
+    handleChange: ChangeEventHandler<HTMLInputElement>;
     zip: string;
-    handleClick: MouseEventHandler<HTMLButtonElement>
+    handleClick: MouseEventHandler<HTMLButtonElement>;
+    errors: string[];
 }
 
-const SearchFormDisplay: FC<Props> = ({ handleChange, handleClick, zip }): ReactElement => {
+const SearchFormDisplay: FC<Props> = ({ handleChange, handleClick, zip, errors }): ReactElement => {
+    const style = errors.length > 0 ? "has-error" : "";
     return (
         <ClayForm id="userSearchBy">
-            <ClayForm.Group>
+            <ClayForm.Group className={style}>
                 <ClayInput
                     id="zipInput"
                     name="zip"
@@ -19,6 +21,19 @@ const SearchFormDisplay: FC<Props> = ({ handleChange, handleClick, zip }): React
                     value={zip}
                     type="text"
                 />
+                <ClayForm.FeedbackGroup>
+                    {errors.map((error, index) => {
+                        switch (error) {
+                            case "inlcudesNonDigits":
+                                return <ClayForm.FeedbackItem key={index}>{Liferay.Language.get("inlcudesNonDigits")}</ClayForm.FeedbackItem>
+                            case "notFiveDigits":
+                                return <ClayForm.FeedbackItem key={index}>{Liferay.Language.get("notFiveDigits")}</ClayForm.FeedbackItem>
+                            default:
+                                return;
+                        }
+                    }
+                    )}
+                </ClayForm.FeedbackGroup>
             </ClayForm.Group>
             <Button onClick={handleClick} displayType="primary">
                 {Liferay.Language.get("search")}
