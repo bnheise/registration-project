40efa1d2cfa8a0027275aec27e3034d216f8fa7f
Brian Heise <brian.heise@liferay.com>
2021-11-22 16:41:16 +0900

    implementd page change request

--- modules/search/search-web-input/src/components/SearchForm/SearchForm.tsx
+++ modules/search/search-web-input/src/components/SearchForm/SearchForm.tsx
@@ -1,16 +1,31 @@
-import React, { FC, ReactElement, useState } from 'react'
-import { UserAddress } from '../../domain/UserAddress';
+import React, { FC, ReactElement, useEffect, useState } from 'react'
 import { handleChange, handleClick } from './handlers';
 import SearchFormDisplay from './SearchFormDisplay';
+
+interface PageChangeRequestData {
+    start: number;
+    end: number;
+    zip: string;
+}
 
 const SearchForm: FC = (): ReactElement => {
     const [zip, setZip] = useState<string>("");
     const [start, setStart] = useState<number>(0);
     const [end, setEnd] = useState<number>(5);
+    const [errors, setErrors] = useState<Array<string>>([]);
+
+    useEffect(() => {
+        Liferay.on("pageChangeRequested", ({ start: newStart, end: newEnd, zip: newZip }: PageChangeRequestData) => {
+            setStart(newStart);
+            setEnd(newEnd);
+            setZip(newZip);
+            handleClick(newZip, newStart, newEnd, setErrors)();
+        })
+    }, []);
 
     return (
         <div>
-            <SearchFormDisplay zip={zip} handleClick={handleClick(zip, start, end)} handleChange={handleChange(setZip)} />
+            <SearchFormDisplay errors={errors} zip={zip} handleClick={handleClick(zip, start, end, setErrors)} handleChange={handleChange(setZip)} />
         </div>
     )
 }
